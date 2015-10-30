5bf2815666acc8b765d269591e77e71d9b2152a2
nefanov <kolya-efan@mail.ru>
2015-10-30 04:42:44 +0300

    release 2 :)
    ''' How to use:
    
    Choose dithering algo
    Enter the filename
    Then chose threshold walue or matrix of dithering (in algo 3)
    Press enter and observe results in output file 'out.jpg'
    
    Set of algorithms:
    1. Fixed threshold
    2. Random threshold
    3. Dithering matrix
    4. Error diffusion by string
    5. Error diffusion by cross-string
    6. Error diffusion by matrix & cross
    
    example of input:
    2
    'lena.jpg'
    128
    
    '''

--- dg.py
+++ dg.py
@@ -1,18 +1,38 @@
 import random
 from PIL import Image, ImageDraw 
 import itertools
+''' How to use:
+
+Choose dithering algo
+Enter the filename
+Then chose threshold walue or matrix of dithering (in algo 3)
+Press enter and observe results in output file 'out.jpg'
+
+Set of algorithms:
+1. Fixed threshold
+2. Random threshold
+3. Dithering matrix
+4. Error diffusion by string
+5. Error diffusion by cross-string
+6. Error diffusion by matrix & cross
+
+example of input:
+2
+'lena.jpg'
+128
+
+'''
+
+threshold_matrix=[[[0.2,0.6],[0.8,0.4]],[[0.1,0.8,0.4],[0.7,0.6,0.3],[0.5,0.2,0.9]]]
 
 mode = int(input('mode:=')) #read arg as num
-#fd = input('image:')
-image = Image.open("lena.jpg")  
+fd = input('image:')
+image = Image.open(fd)  
 draw = ImageDraw.Draw(image) #draw instrument
 pix = list(image.getdata())
 width, height = image.size
 pix = [pix[i * width:(i + 1) * width] for i in xrange(height)]
 
-print pix[1][1]
-#pix = [pix[i*width:(i+1) * width] for i in xrange(height)]
-#pix = list(itertools.chain(*pix))
 for i in range(height):
 	for j in range(width):
 		pix[i][j] = list(pix[i][j])
@@ -22,7 +42,7 @@ if (mode == 1): #b/w with user-setting factor
 	for i in range(width):
 		for j in range(height):
 			S = sum(pix[i][j])
-			if (S > (((255 + factor) // 2) * 3)):
+			if (S > (factor) * 3):
 				a, b, c = 255, 255, 255
 			else:
 				a, b, c = 0, 0, 0
@@ -34,34 +54,23 @@ if (mode == 2): #b/w with per-pixel randomized factor
 
 			factor = random.randrange(0,255,1)
 			S = sum(pix[i][j])
-			if (S > (((255 + factor) // 2) * 3)):
+			if (S > ((factor) * 3)):
 				a, b, c = 255, 255, 255
 			else:
 				a, b, c = 0, 0, 0
 			draw.point((j, i), (a, b, c))
 
-if (mode == 3): #ordered dithering
-
-	order_fn = input('order_matrix:=')
-	order_fd = open(order_fn,'r')
-	A=[]
-	orders = (order_fd.read()).split("\n")
-	for i in range(len(orders)-1):
-		A.append(map(int,orders[i].split()))
-	orders = A
-	print orders
-
-	for i in range(width):
-		for j in range(height):
-
-			factor = random.randrange(0,255,1)
+if (mode == 3): #ordered dithering on matrix
+	num = int(input('Number of dithering matrix (1,2):='))
+	for i in range(height):
+		for j in range(width):
 			S = sum(pix[i][j])
-			if (S > (((255 + orders[i][j]) // 2) * 3)):
+			if ( ((255*3) // 2) < (S+S*threshold_matrix[num-1][i % len(threshold_matrix[num-1][0])][j % len(threshold_matrix[num-1])])):
 				a, b, c = 255, 255, 255
 			else:
 				a, b, c = 0, 0, 0
 			draw.point((j, i), (a, b, c))
-
+			
 if (mode == 4):#string diff
 	factor = int(input('factor:='))
 	for j in range(height):
@@ -69,8 +78,7 @@ if (mode == 4):#string diff
 		for i in range(width-1):
 			oldpix = pix[i][j]
 			S = sum(pix[i][j])
-			print pix[i][j]
-			if (S > (((255 + factor) //2)*3)):
+			if (S > ( factor)*3):
 				pix[i][j][0] = pix[i][j][1] = pix[i][j][2] = 255#; [255,255,255]
 			else:
 				pix[i][j][0] = pix[i][j][1] = pix[i][j][2] = 0#;
@@ -82,55 +90,84 @@ if (mode == 4):#string diff
 
 if (mode == 5):#cross diff
 	factor = int(input('factor:='))
-	for j in range(height):
-		i = 1
-		for i in range(width-1):
-			oldpix = pix[i][j]
-			S = sum(pix[i][j])
-			if (S > (((255 + factor) //2)*3)):
-				pix[i][j] = [255,255,255]
-			else:
-				pix[i][j] = [0,0,0]
-
-			quant_error = sum(oldpix) - sum(pix[i][j])
-			for k in range(3):	
-				if (j % 2):
-			
-					pix[i+1][j][k] = pix[i+1][j][k] + quant_error * 7/16
+	for i in range(height):
+		if not (i % 2):
+			j = 1
+			for j in range(width-1):
+				oldpix = pix[i][j]
+				S = sum(pix[i][j])
+				if (S >  (factor)*3):
+					pix[i][j] = [255,255,255]
 				else:
-					pix[width-i-1][j][k] = pix[width-i-1][j][k] + quant_error * 7/16
-			draw.point((j, i), (pix[i][j][0], pix[i][j][1], pix[i][j][2]))
+					pix[i][j] = [0,0,0]
 
-if (mode == 6):#floyd-steinberg 
+				quant_error = (sum(oldpix) - sum(pix[i][j]))//3
+				for k in range(3):	
+					pix[i-1][j][k] = pix[i-1][j][k] + quant_error * 7/16
+				draw.point((j, i), (pix[i][j][0], pix[i][j][1], pix[i][j][2]))
+		else:
+			j = height - 1
+			while j > 0:
+				oldpix = pix[i][j]
+				S = sum(pix[i][j])
+				if (S > (factor)*3):
+					pix[i][j] = [255,255,255]
+				else:
+					pix[i][j] = [0,0,0]
+
+				quant_error = sum(oldpix) - sum(pix[i][j])
+				for k in range(3):	
+					pix[i-1][j][k] = pix[i-1][j][k] + quant_error * 7/16
+				draw.point((j, i), (pix[i][j][0], pix[i][j][1], pix[i][j][2]))
+				j-=1
+
+if (mode == 6):#floyd-steinberg
 	factor = int(input('factor:='))
-	for j in range(height):
-		i=1
-		for i in range(width-1):
-			oldpix = pix[i][j]
-			S = sum(pix[i][j])
-			if (S > (((255 + factor) //2)*3)):
-				pix[i][j] = [255,255,255]
-			else:
-				pix[i][j] = [0,0,0]
-
-			quant_error = sum(oldpix) - sum(pix[i][j])
-			for k in range(3):
-				if (j == height - 1):
-					if (j % 2):
-						pix[i+1][j][k] = pix[i+1][j][k] + quant_error * 7/16
-					else:
-						pix[i-1][j][k] = pix[i-1][j][k] + quant_error * 5 /16
-				elif (j % 2):
-					pix[i+1][j][k] = pix[i+1][j][k] + quant_error * 7/16
-					pix[i-1][j+1][k] = pix[i-1][j+1][k] + quant_error * 3/16
-					pix[i][j+1][k] = pix[i][j+1][k] + quant_error * 5/16
-					pix[i+1][j+1][k] = pix [i+1][j+1][k] + quant_error /16
+	for i in range(height-1):
+		if not (i % 2):
+			j = 1
+			for j in range(width-1):
+				oldpix = pix[i][j]
+				S = sum(pix[i][j])
+				if (S > (factor*3)):
+					pix[i][j] = [255,255,255]
 				else:
-					pix[i-1][j][k] = pix[i-1][j][k] + quant_error * 5 /16
-					pix[i-1][j+1][k] = pix [i-1][j+1][k] + quant_error * 3/16
-					pix[i][j+1][k] = pix[i][j+1][k] + quant_error * 7/16
-					pix[i+1][j+1][k] = pix[i][j+1][k] + quant_error * 1/16
+					pix[i][j] = [0,0,0]
 
-			draw.point((j, i), (pix[i][j][0], pix[i][j][1], pix[i][j][2]))
+				quant_error = (sum(oldpix) - sum(pix[i][j]))//3
+				if (i == height - 1):
+					for k in range(3):	
+						pix[i][j+1][k] = pix[i][j+1][k] + quant_error * 7/16
+				else:
+					for k in range(3):
+						pix[i][j+1][k] = pix[i][j+1][k] + quant_error * 7/16
+						pix[i+1][j-1][k] = pix[i+1][j-1][k] + quant_error * 3/16
+						pix[i+1][j][k] = pix[i+1][j][k] + quant_error * 5/16
+						pix[i+1][j+1][k] = pix [i+1][j+1][k] + quant_error /16	
+					
+				draw.point((j, i), (pix[i][j][0], pix[i][j][1], pix[i][j][2]))
+		else:
+			j = width - 2
+			while j > 0:
+				oldpix = pix[i][j]
+				S = sum(pix[i][j])
+				if (S > (factor)*3): 
+					pix[i][j] = [255,255,255]
+				else:
+					pix[i][j] = [0,0,0]
+
+				quant_error = (sum(oldpix) - sum(pix[i][j]))//3
+				if (i == height - 1):
+					for k in range(3):	
+						pix[i][j-1][k] = pix[i][j-1][k] + quant_error * 7/16
+				else:
+					for k in range(3):
+						pix[i][j-1][k] = pix[i][j-1][k] + quant_error * 5 /16
+						pix[i+1][j-1][k] = pix[i+1][j-1][k] + quant_error * 3/16
+						pix[i+1][j][k] = pix[i+1][j][k] + quant_error * 7/16
+						pix[i+1][j+1][k] = pix[i+1][j][k] + quant_error * 1/16	
+				draw.point((j, i), (pix[i][j][0], pix[i][j][1], pix[i][j][2]))
+				j-=1
+
 image.save("out.jpg", "JPEG")
 del draw
